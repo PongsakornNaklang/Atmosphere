6c306d1bd4ea2bc7b71a62e8652cf6d38bd3871f
PongsakornNaklang <PongsakornNaklang03@gmail.com>
2020-05-04 19:48:02 +0700

    make it better2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const { getHomepage } = require('./routes/index');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.set('port', process.env.port || port);
 app.set('views', __dirname + '/views');
